05a16a55f037090684680ca23f15d1f4f7d7acaa
VladimirAz <vladimir.azarov1996@gmail.com>
2024-04-01 09:52:43 +0300

    Дз logging-1

--- docker/docker-monolith/infra/terraform/backend.tf
+++ docker/docker-monolith/infra/terraform/backend.tf
@@ -4,9 +4,8 @@ terraform {
     bucket   = "reddit-bucket"
     region   = "ru-central1"
     key      = "terraform.tfstate"
-    access_key = "YCAJER-gM1GXuRaBGsEPElcHJ"
-    secret_key = "YCPdQb_hRFCOrSWTrR_yEnzrAtLo0PBca2Q_EMb"
-
+    access_key = "YCAJER-gMjfkjvhfdkvjhcHJ"
+    secret_key = "Yehwkjrb_hRh3g4hj343bhbk2
     skip_region_validation      = true
     skip_credentials_validation = true
   }
